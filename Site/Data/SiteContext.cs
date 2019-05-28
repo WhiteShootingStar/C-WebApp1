37cd58929425b66b721ac7de47129db044843854
Golbez <palart2000@gmail.com>
2019-05-28 19:32:34 +0200

    initial commit

---
+++ Site/Data/SiteContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Site.Models
+{
+    public class SiteContext : DbContext
+    {
+        public SiteContext (DbContextOptions<SiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Site.Models.Movie> Movie { get; set; }
+    }
+}
